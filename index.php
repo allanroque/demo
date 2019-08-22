b0c7a714b14d2c047ed2ae9923efbf66b7d12a0c
Allan Roque <allanrafaelroque@gmail.com>
2019-08-22 11:01:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 </head>
 <body>
     <div class="header">
-        Red Hat Red Hat !!!
+        Red Hat
     </div>
     <img class="logo" src="https://www.underconsideration.com/brandnew/archives/red_hat_logo_inverse.png">
 </body>
